dbf7e0f9a56d885a34711e7d90ae2326f14d7b68
platonov-platonovich <109359331+platonov-platonovich@users.noreply.github.com>
2022-12-14 14:38:01 +0300

    feign (stop)

--- src/main/java/app/CryptoApplication.java
+++ src/main/java/app/CryptoApplication.java
@@ -2,9 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+//import org.springframework.cloud.netflix.feign.EnableFeignClients;
+//import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@EnableFeignClients
+//@EnableFeignClients
 @SpringBootApplication
 public class CryptoApplication {
     public static void main(String[] args) {
--- src/main/java/app/FeignClient.java
+++ src/main/java/app/FeignClient.java
@@ -1,4 +1,20 @@
-package app;
-
-public interface FeignClient {
-}
+//package app;
+//
+//import app.models.Post;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+//import static org.springframework.web.bind.annotation.RequestMethod.GET;
+//import static org.springframework.web.bind.annotation.RequestMethod.POST;
+//
+//
+//@org.springframework.cloud.netflix.feign.FeignClient(value = "jplaceholder", url = "https://api.coinlore.net/api/ticker/")
+//public interface FeignClient {
+//    @RequestMapping(method = RequestMethod.GET, value = "/posts")
+//    List<Post> getPosts();
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/{postId}", produces = "application/json")
+//    Post getPostById(@PathVariable("postId") Long postId);
+//}
